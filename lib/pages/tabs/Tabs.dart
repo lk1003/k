2b3a2583193c86d90b2a52243fb80ceb933d36c0
刘可 <591361969@qq.com>
2020-01-06 14:20:26 +0800

    1.首页列表

---
+++ lib/pages/tabs/Tabs.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:jdshop/pages/tabs/Cart.dart';
+import 'package:jdshop/pages/tabs/Category.dart';
+import 'package:jdshop/pages/tabs/Home.dart';
+import 'package:jdshop/pages/tabs/User.dart';
+
+class Tabs extends StatefulWidget {
+  Tabs({Key key}) : super(key: key);
+
+  @override
+  _TabsState createState() => _TabsState();
+}
+
+class _TabsState extends State<Tabs> {
+  int _currentIndex=0;
+
+  List _pageList=[
+    HomePage(),
+    CategoryPage(),
+    CartPage(),
+    UserPage()
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("京东商城"),
+      ),
+      body: this._pageList[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: this._currentIndex,
+        onTap: (index){
+          setState(() {
+            this._currentIndex=index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        fixedColor: Colors.red,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text("首页")
+          ),BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            title: Text("分类")
+          ),BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            title: Text("购物车")
+          ),BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            title: Text("我的")
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/services/ScreenAdapter.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ScreenAdapter {
+  static init(context) {
+    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
+  }
+
+  static height(double height) {
+    return ScreenUtil.getInstance().setHeight(height);
+  }
+
+  static width(double width) {
+    return ScreenUtil.getInstance().setWidth(width);
+  }
+
+  static getScreenHeight() {
+    return ScreenUtil.screenHeightDp;
+  }
+
+  static getScreenWidth() {
+    return ScreenUtil.screenWidthDp;
+  }
+}
